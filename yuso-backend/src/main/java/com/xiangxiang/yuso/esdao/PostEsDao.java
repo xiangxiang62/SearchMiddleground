4138fb0c89d29f6032bc1c8b32afd569a1654c30
xiangxiang62 <1964362087@qq.com>
2024-02-06 20:01:33 +0800

    项目初始化

---
+++ yuso-backend/src/main/java/com/xiangxiang/yuso/esdao/PostEsDao.java
@@ -0,0 +1,14 @@
+package com.xiangxiang.yuso.esdao;
+
+import com.xiangxiang.yuso.model.dto.post.PostEsDTO;
+
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
---
+++ yuso-backend/src/main/java/com/xiangxiang/yuso/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.xiangxiang.yuso.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiangxiang.yuso.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
